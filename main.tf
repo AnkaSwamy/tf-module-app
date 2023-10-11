9d141b976a23bf6b35360aacd5bfc09c6eefe5a6
Anka swamy <ankaswamy.telagorla@gmail.com>
2023-10-11 17:11:05 +0530

    Makefile is used to overcome enter every time terraform commands

--- main.tf
+++ main.tf
@@ -4,18 +4,18 @@ resource "aws_iam_policy" "policy" {
   description = "${var.component}-${var.env}-ssm-pm-policy"
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        "Sid" : "VisualEditor0",
-        "Effect" : "Allow",
-        "Action" : [
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : "arn:aws:ssm:us-east-1:318042291266:parameter/roboshop.${var.env}.${var.component}.*"
+        "Resource": "arn:aws:ssm:us-east-1:318042291266:parameter/roboshop.${var.env}.${var.component}.*"
       }
     ]
   })
